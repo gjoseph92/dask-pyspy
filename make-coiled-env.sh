fe6754a3766885f875e69a58d0423838e74c94a7
Gabe Joseph <gjoseph92@gmail.com>
2021-05-06 12:36:34 -0600

    update make-coiled-env script to pin commit

--- make-coiled-env.sh
+++ make-coiled-env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+LATEST_COMMIT=$(git rev-parse origin/main)
 poetry export --without-hashes -E docker > requirements.txt
-echo "git+https://github.com/gjoseph92/distributed-pyspy.git" >> requirements.txt
+echo "git+https://github.com/gjoseph92/distributed-pyspy.git@$LATEST_COMMIT" >> requirements.txt
 coiled env create -n pyspy --pip requirements.txt --private
 rm requirements.txt
